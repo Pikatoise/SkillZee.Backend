1ef37bd099d77655e28d30e9e1cefda3d6ed9117
Pikatoise <wierwolf@inbox.ru>
2024-12-30 19:54:38 +0500

    Create BaseEntity and User

---
+++ SkillZee.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace SkillZee.Domain.Entities
+{
+    public class BaseEntity
+    {
+        public Guid Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public bool IsActive { get; set; } = true;
+    }
+}
---
+++ SkillZee.Domain/Entities/User.cs
@@ -0,0 +1,19 @@
+﻿namespace SkillZee.Domain.Entities
+{
+    public class User: BaseEntity
+    {
+        public string Email { get; set; } = null!;
+
+        public string Password { get; set; } = null!;
+
+        public string Nickname { get; set; } = null!;
+
+        public bool IsWorker { get; set; } = false;
+
+        public int SuccessOrders { get; set; }
+
+        public string Avatar { get; set; } = null!;
+
+        public DateTime LastOnline { get; set; }
+    }
+}
