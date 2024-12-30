74d8eadaa27ac8d5aac5a67b71a84ede1e7e7c00
Pikatoise <wierwolf@inbox.ru>
2024-12-30 19:18:00 +0500

    Init DbContext

---
+++ SkillZee.Infrastructure/DAL/SkillZeeDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SkillZee.Infrastructure.DAL;
+
+public sealed class SkillZeeDbContext: DbContext
+{
+    public SkillZeeDbContext(DbContextOptions<SkillZeeDbContext> options) : base(options)
+    {
+        if (Database.GetPendingMigrations().Any())
+        {
+            Database.Migrate();
+        }
+    }
+}
