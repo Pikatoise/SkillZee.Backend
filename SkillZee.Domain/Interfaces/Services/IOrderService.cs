95669cbbcd9dfd8c7f54a835363227f3a3826525
Pikatoise <wierwolf@inbox.ru>
2025-01-08 16:48:33 +0500

    Another method to IOrderService

--- SkillZee.Domain/Interfaces/Services/IOrderService.cs
+++ SkillZee.Domain/Interfaces/Services/IOrderService.cs
@@ -6,5 +6,7 @@ namespace SkillZee.Domain.Interfaces.Services
     public interface IOrderService: IBaseEntityService<OrderDto>
     {
         Task<BaseResult<OrderDto>> CreateOrder(CreateOrderDto dto);
+
+        Task<BaseResult<bool>> ResponseToOrder(Guid orderId, Guid workerId);
     }
 }
