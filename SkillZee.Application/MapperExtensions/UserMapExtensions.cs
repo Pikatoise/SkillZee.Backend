2c1f0ba35438b826648acc76b46897c5aa24eb49
Pikatoise <wierwolf@inbox.ru>
2025-01-09 19:33:07 +0500

    Completly fix null warning

--- SkillZee.Application/MapperExtensions/UserMapExtensions.cs
+++ SkillZee.Application/MapperExtensions/UserMapExtensions.cs
@@ -15,7 +15,7 @@ namespace SkillZee.Application.MapperExtensions
             IsActive = user.IsActive,
             IsWorker = user.IsWorker,
             SuccessOrders = user.SuccessOrders,
-            WorkerInfo = user.WorkerInfo.ToDto()
+            WorkerInfo = user.WorkerInfo != null ? user.WorkerInfo.ToDto() : null
         };
     }
 }
