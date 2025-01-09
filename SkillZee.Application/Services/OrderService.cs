6f4f279037cc66a57bbc0e80d756d4843393e026
Pikatoise <wierwolf@inbox.ru>
2025-01-09 18:53:22 +0500

    Add base of OrderService

---
+++ SkillZee.Application/Services/OrderService.cs
@@ -0,0 +1,44 @@
+﻿using SkillZee.Domain;
+using SkillZee.Domain.DTO.Order;
+using SkillZee.Domain.Filters;
+using SkillZee.Domain.Interfaces.Services;
+using SkillZee.Domain.Result;
+using SkillZee.Infrastructure.DAL.Repositories;
+
+namespace SkillZee.Application.Services
+{
+    public class OrderService(UnitOfWork unitOfWork): IOrderService
+    {
+        UnitOfWork _unitOfWork = unitOfWork;
+
+        public Task<BaseResult<OrderDto>> CreateOrder(CreateOrderDto dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<BaseResult<OrderDto>> Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PagedResult<OrderDto>> GetAllOrders(OrderFilter filters, SortParams sorting, PageParams paging)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<BaseResult<OrderDto>> GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<BaseResult<bool>> ResponseToOrder(Guid orderId, Guid workerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<BaseResult<OrderDto>> Turn(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
