45ba33a98bfa95d7d8c7d605884f363850a5647a
Pikatoise <wierwolf@inbox.ru>
2025-01-09 18:46:06 +0500

    Add data for orderSpeed configuration

--- SkillZee.Infrastructure/DAL/Configurations/OrderSpeedConfiguration.cs
+++ SkillZee.Infrastructure/DAL/Configurations/OrderSpeedConfiguration.cs
@@ -13,6 +13,25 @@ namespace SkillZee.Infrastructure.DAL.Configurations
 
             builder.Property(x => x.Title).IsRequired().HasMaxLength(20);
             builder.Property(x => x.RewardMultiplier).IsRequired().HasDefaultValue(1.0);
+
+            builder.HasData(new List<OrderSpeed>()
+            {
+                new OrderSpeed()
+                {
+                    Title = "Стандартно",
+                    RewardMultiplier = 1.0
+                },
+                new OrderSpeed()
+                {
+                    Title = "Срочно",
+                    RewardMultiplier = 1.1
+                },
+                new OrderSpeed()
+                {
+                    Title = "Очень срочно",
+                    RewardMultiplier = 1.2
+                }
+            });
         }
     }
 }
